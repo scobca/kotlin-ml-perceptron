ed02bfb12049243ee5b83f1c41272c5a750de478
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-12 02:08:50 +0300

    feat(core): created ProfessionVectors.kt

---
+++ src/main/kotlin/org/itmo/kotlinmlperceptron/structure/ProfessionVectors.kt
@@ -0,0 +1,7 @@
+package org.itmo.kotlinmlperceptron.structure
+
+object ProfessionVectors {
+    val designer = doubleArrayOf(1.0, 0.0, 0.0)
+    val frontend = doubleArrayOf(0.0, 1.0, 0.0)
+    val backend = doubleArrayOf(0.0, 0.0, 1.0)
+}
